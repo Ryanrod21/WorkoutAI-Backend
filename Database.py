39275319c466b563b2f71c7a95bb8370ff41666e
Ryan Rodriguez <ryancode2321@gmail.com>
2026-01-24 14:15:34 -0600

    conflict update

--- Database.py
+++ Database.py
@@ -51,12 +51,12 @@ def upsert_plans(user_id: UUID, new_plans: list):
 
 def upsert_progression(user_id: UUID, week: int, progression_data: dict):
     supabase.table("gym").upsert({
-        "user_id": str(user_id),
-        "week": week,
-        "day_status": progression_data.get("day_status"),
-        "difficulty": progression_data.get("difficulty"),
-        "soreness": progression_data.get("soreness"),
-        "completed": progression_data.get("completed"),
-        "progression": progression_data.get("progression"),
-        "feedback": progression_data.get("feedback"),
-    }, on_conflict=["user_id","week"]).execute()
+    "user_id": str(user_id),
+    "week": week,
+    "day_status": progression_data.get("day_status"),
+    "difficulty": progression_data.get("difficulty"),
+    "soreness": progression_data.get("soreness"),
+    "completed": progression_data.get("completed"),
+    "progression": progression_data.get("progression"),
+    "feedback": progression_data.get("feedback"),
+}, on_conflict="user_week_unique").execute()
